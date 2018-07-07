9d0a9cf9df6e0ee02004d30a2970145d0a388d2c
Claire <claire0505@gmail.com>
2018-07-07 17:36:23 +0800

    04 建立擴充類別Product並新增ProductMetadata類別並加入資料驗證

---
+++ MVC5Course/Models/Partial/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC5Course.Models
+{
+    [MetadataType(typeof(ProductMetadata))]
+    public partial class Product
+    {
+        private class ProductMetadata
+        {
+            public int ProductId { get; set; }
+
+            [Required(ErrorMessage = "商品名稱為必填")]
+            [StringLength(20, ErrorMessage = "商名名稱不得大於20個字")]
+            [Display(Name = "產品名稱")]
+            public string ProductName { get; set; }
+
+            [Required]
+            [DisplayFormat(DataFormatString = "{0:N0}")]
+            public Nullable<decimal> Price { get; set; }
+
+            [Required]
+            public Nullable<bool> Active { get; set; }
+
+            [Required]
+            public Nullable<decimal> Stock { get; set; }
+        }
+    }
+}
