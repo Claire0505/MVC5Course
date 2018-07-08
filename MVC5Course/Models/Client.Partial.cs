2c5d58e0008d8898c4b38679b772fa5e0f20af0d
Claire <claire0505@gmail.com>
2018-07-09 01:10:12 +0800

    示範如何調整「日期」欄位的 type 屬性如何變更

--- MVC5Course/Models/Client.Partial.cs
+++ MVC5Course/Models/Client.Partial.cs
@@ -44,6 +44,7 @@ namespace MVC5Course.Models
         public string Gender { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
 
         [Required]
