b856e3ca6930a058404de14f130f82d1e05755b4
Claire <claire0505@gmail.com>
2018-07-09 01:02:02 +0800

    13 套用 Model 的「輸入驗證」與「模型驗證」 ( Client )
    資料庫結構更新：
    1. 將 Client 資料表中所有 varchar 改成 nvarchar
    2. 在 Client 資料表加入 IdNumber char(10) 欄位
    
    修正模型類別:
    1. 修改 DateOfBirth 欄位屬性，將顯示格式改為 yyyy-MM-dd
    2. 在 Client 的 IdNumber 套用「輸入驗證」=> 身份證字號Attribute
    2. 在 Client 的 姓名與性別欄位 套用「輸入驗證」=> Required
    3. 在 Client 實作模型驗證，確保 Longitude 與 Latitude 欄位都要一起設定或一起不設定

--- MVC5Course/Models/Client.Partial.cs
+++ MVC5Course/Models/Client.Partial.cs
@@ -3,6 +3,7 @@ namespace MVC5Course.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using Models.InputValidations;
 
     [MetadataType(typeof(ClientMetaData))]
     public partial class Client : IValidatableObject
@@ -10,10 +11,17 @@ namespace MVC5Course.Models
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             //實作模型驗證
-            if (this.DateOfBirth.Value.Year > 1980 && this.City == "Taipei")
+            if (this.Longitude.HasValue != this.Latitude.HasValue)
             {
-                yield return new ValidationResult("條件錯誤", new string[] { "DateOfBirth", "City" });
+                yield return new ValidationResult("經緯度欄位必須一起設定", new string[] { "Longitude", "Latitude" });
             }
+
+            // 模型驗證可以放入多組 下方為示範
+            // 模型驗證為後端驗證，因此送出後才會顯示錯誤訊息內容
+            //if (this.DateOfBirth.Value.Year > 1980 && this.City == "Taipei")
+            //{
+            //    yield return new ValidationResult("條件錯誤", new string[] { "DateOfBirth", "City" });
+            //}
         }
     }
 
@@ -24,16 +32,21 @@ namespace MVC5Course.Models
         
         [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
         public string FirstName { get; set; }
-        
+               
         [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
         public string MiddleName { get; set; }
-        
+
+        [Required]
         [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
         public string LastName { get; set; }
         
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
+
+        [Required]
         public Nullable<double> CreditRating { get; set; }
         
         [StringLength(7, ErrorMessage="欄位長度不得大於 7 個字元")]
@@ -57,7 +70,10 @@ namespace MVC5Course.Models
         public Nullable<double> Longitude { get; set; }
         public Nullable<double> Latitude { get; set; }
         public string Notes { get; set; }
-    
+
+        [IdentificationId]
+        public string IdNumber { get; set; }
+
         public virtual Occupation Occupation { get; set; }
         public virtual ICollection<Order> Order { get; set; }
     }
