a8a72c9f4df354c79f80181a60b39654cca28e1e
Claire <claire0505@gmail.com>
2018-07-08 23:08:56 +0800

    12 套用模型類別的 MetadataType 類別，使用 EFGeneratePartialClass.tt

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,39 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        [Display(Name = "商品編號")]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "商品名稱")]
+        [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [Display(Name = "價格")]
+        [Range(10, 99999)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public Nullable<decimal> Price { get; set; }
+
+        [Required]
+        [Display(Name = "是否有效")]
+        public Nullable<bool> Active { get; set; }
+
+        [Required]
+        [Display(Name = "庫存")]
+        public Nullable<decimal> Stock { get; set; }
+
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
