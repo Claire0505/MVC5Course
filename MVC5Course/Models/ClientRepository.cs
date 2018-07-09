309558f48bf334578114c79f02b4e3d5caf3baa0
Claire <claire0505@gmail.com>
2018-07-09 15:53:44 +0800

    16 請修改 ClientsController 的刪除功能，讓資料庫「標示已刪除」即可，不要真的刪除資料 資料庫結構更新： 在 Client 資料表加入 Active Bit 欄位

--- MVC5Course/Models/ClientRepository.cs
+++ MVC5Course/Models/ClientRepository.cs
@@ -6,6 +6,19 @@ namespace MVC5Course.Models
 {
     public class ClientRepository : EFRepository<Client>, IClientRepository
     {
+        //16 請修改 ClientsController 的刪除功能，讓資料庫「標示已刪除」即可，不要真的刪除資料
+        public IQueryable<Client> All (bool isAll = false)
+        {
+            var data = base.All();
+
+            if (!isAll)
+            {
+                data = base.All().Where(p => p.CreditRating < 2 && p.Active == false);
+            }
+
+            return data;
+        }
+
         public IQueryable<Client> SearchKeyword(string keyword)
         {
             var data = this.All();
@@ -21,6 +34,11 @@ namespace MVC5Course.Models
         internal Client Find(int id)
         {
             return this.All().FirstOrDefault(f => f.ClientId == id);
+        }
+
+        public override void Delete(Client entity)
+        {
+            entity.Active = false;
         }
     }
 
