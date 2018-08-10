526c014e71de3dad9c838e1d551f3b6aecaf37f6
Claire <claire0505@gmail.com>
2018-08-10 16:43:02 +0800

    示範進階的 Model Binding 用法

---
+++ MVC5Course/Models/PageCondViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class PageCondViewModel
+    {
+        public string Keyword { get; set; }
+        public int PageNo { get; set; }
+
+    }
+}
