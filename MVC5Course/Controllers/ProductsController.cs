b01a5b5a5e84f1f3b6467eeef3fdfd9f79ac8ae5
Claire <claire0505@gmail.com>
2018-08-23 21:42:03 +0800

    示範 LoadingElementId 的作法

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Threading;
 using System.Web.Mvc;
 using MVC5Course.Models;
 using Omu.ValueInjecter;
